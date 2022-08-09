439f93028a454840814cf9cd4ed4a40381793dc0
“xiaobao2333” <1524573182@qq.com>
2022-08-09 22:57:02 +0800

    first commit

--- ACwing/周赛/第62场周赛/A.AcWing 4500. 三个元素.cpp
+++ ACwing/周赛/第62场周赛/A.AcWing 4500. 三个元素.cpp
@@ -1,47 +1,47 @@
-// #include <iostream>
-// #include <cstring>
-// #include <algorithm>
-// using namespace std;
-// const int N = 3010;
-// int a[N];
-// int main()
-// {
-//     int minn = 1e9 + 10, midd = -1, maxx = -1;
-//     int flag1 = 0, flag2 = 0, flag3 = 0;
-//     int n;
-//     cin >> n;
-//     for (int i = 1; i <= n; i++)
-//     {
-//         cin >> a[i];
-//         if (a[i] <= minn)
-//         {
-//             minn = a[i];
-//             flag1 = i;
-//         }
-//         if (a[i] >= maxx)
-//         {
-//             maxx = a[i];
-//             flag3 = i;
-//         }
-//     }
-//     for (int i = 1; i <= n; i++)
-//     {
-//         if (a[i] > minn && a[i] < maxx)
-//         {
-//             flag2 = i;
-//             break;
-//         }
-//     }
-//     if (flag1 && flag2 && flag3)
-//     {
-//         cout << flag1 << " " << flag2 << " " << flag3 << endl;
-//     }
-//     else
-//     {
-//         cout << "-1 -1 -1" << endl;
-//     }
-//     return 0;
-// }
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+const int N = 3010;
+int a[N];
+int main()
+{
+    int minn = 1e9 + 10, midd = -1, maxx = -1;
+    int flag1 = 0, flag2 = 0, flag3 = 0;
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        if (a[i] <= minn)
+        {
+            minn = a[i];
+            flag1 = i;
+        }
+        if (a[i] >= maxx)
+        {
+            maxx = a[i];
+            flag3 = i;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (a[i] > minn && a[i] < maxx)
+        {
+            flag2 = i;
+            break;
+        }
+    }
+    if (flag1 && flag2 && flag3)
+    {
+        cout << flag1 << " " << flag2 << " " << flag3 << endl;
+    }
+    else
+    {
+        cout << "-1 -1 -1" << endl;
+    }
+    return 0;
+}
 //基本思路是找到最大最小值记录下坐标，然后找是否存在中间值，如果存在中间值，那么进行输出答案，否则输出“-1 -1 -1”
 //第二种思路：
 /*#include <iostream>
