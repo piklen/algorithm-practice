9d9f028f01cc8be2000f2ed2ee7217280c44d9e2
xiaobao <1524573182@qq.com>
2022-08-10 15:16:57 +0800

    commit

--- ACwing/算法基础课/基础算法/AcWing 800.数组元素的目标和.cpp
+++ ACwing/算法基础课/基础算法/AcWing 800.数组元素的目标和.cpp
@@ -1,23 +1,31 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-const int N = 1e5 + 10;
-
-int n, m, x;
-int a[N], b[N];
-
+const int N=1e5+10;
+int a[N],b[N];
 int main()
 {
-    scanf("%d%d%d", &n, &m, &x);
-    for (int i = 0; i < n; i ++ ) scanf("%d", &a[i]);
-    for (int i = 0; i < m; i ++ ) scanf("%d", &b[i]);
-
-    for (int i = 0, j = m - 1; i < n; i ++ )
+    int n,m,k;
+    cin>>n>>m>>k;
+    for(int i=0;i<n;i++)
     {
-        while (j >= 0 && a[i] + b[j] > x) j -- ;
-        if (j >= 0 && a[i] + b[j] == x) cout << i << ' ' << j << endl;
+        cin>>a[i];
     }
-
+    for(int j=0;j<m;j++)
+    {
+        cin>>b[j];
+    }
+    int j=m-1;
+    for(int i=0;i<n;i++)
+    {
+        while(j>=0&&a[i]+b[j]>k)
+        {
+            j--;
+        }
+        if(a[i]+b[j]==k)
+        {
+            cout<<i<<" "<<j<<endl;
+            break;
+        }
+    }
     return 0;
 }
